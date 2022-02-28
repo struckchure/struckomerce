1d8d51fe22cc65d4f8ec294979d3c5eca0019169
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-02-28 11:37:49 +0100

    build(heroku): changes psycopg2 to psycopg2-binary in dependencies

--- api/ecommerce/settings.py
+++ api/ecommerce/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "some-radnom-kjn")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.getenv("DEBUG", True))
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(" ")
 
 
 # Application definition
