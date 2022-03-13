d8588fc575747a6a7f25be9ec20ff044fb0bf6f1
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-03-13 17:42:18 +0100

    updates sh script for env file

--- api/ecommerce/settings.py
+++ api/ecommerce/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", "some-radnom-kjn")
+SECRET_KEY = os.environ.get("SECRET_KEY", "some-radnom-kjn")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.getenv("DEBUG", True))
+DEBUG = int(os.environ.get("DEBUG", True))
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split() or ["*"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split()
 
 # CORS configs
 
