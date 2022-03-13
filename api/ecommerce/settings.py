b8b946f5c12f978a44426fee206c7cfb1498ec0e
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-03-13 14:43:07 +0100

    updates settings file

--- api/ecommerce/settings.py
+++ api/ecommerce/settings.py
@@ -29,20 +29,16 @@ SECRET_KEY = os.getenv("SECRET_KEY", "some-radnom-kjn")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.getenv("DEBUG", True))
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(" ")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ") or ["*"]
 
 # CORS configs
 
-CORS_ALLOW_ALL_ORIGINS = (
-    True  # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
-)
-# CORS_ALLOW_CREDENTIALS = True
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:3030',
-# ] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
-# CORS_ALLOWED_ORIGIN_REGEXES = [
-#     'http://localhost:3030',
-# ]
+if DEBUG:
+    CORS_ALLOW_ALL_ORIGINS = True
+else:
+    CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS") or [
+        "http://localhost:3030",
+    ]
 
 # Application definition
 
